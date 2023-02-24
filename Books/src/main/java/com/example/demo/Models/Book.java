47d50cb1fe8aaf366a714b7e091e52f8c09ce807
sreehari17 <101086245+Sreehari1770@users.noreply.github.com>
2023-02-24 11:40:30 +0530

    Portal DAY-5

---
+++ Books/src/main/java/com/example/demo/Models/Book.java
@@ -0,0 +1,39 @@
+package com.example.demo.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	private int id;
+	private String bookName;
+	private float price;
+	private String authorName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	
+	
+}
